d779a2afab5b3337b4ed415c80030b5aaab78641
dylanonelson <dylanonelson@gmail.com>
2016-12-04 15:36:00 -0500

    Group marks by five

--- src/components/LogMarks.js
+++ src/components/LogMarks.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
-const getMarkString = (mark, times) => (new Array(times).fill(mark).join(' '))
+const MARKS_PER_ROW = 5;
+
+const getMarks = (mark, times) => {
+  const rows = Math.floor(times / MARKS_PER_ROW);
+  const remainder = times % MARKS_PER_ROW
+  const lis = [];
+  for (let i = 0; i < rows; i++) {
+    lis.push(
+      <li
+        key={i}
+      >
+        {new Array(5).fill(mark).join('')}
+      </li>
+    )
+  }
+  lis.push(
+    <li
+      key={rows}
+    >
+      {new Array(remainder).fill(mark).join('')}
+    </li>
+  );
+  return (
+    <ul>
+      {lis}
+    </ul>
+  );
+}
 
 const LogMarks = ({ logs, logType }) => {
   return (
     <li>
       <h3>{logType.name}</h3>
-      <p>{getMarkString(logType.mark, logs.length)}</p>
+      {getMarks(logType.mark, logs.length)}
     </li>
   );
 };
