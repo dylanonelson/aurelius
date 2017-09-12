8c81c65a1d3e4f1ee2a970e791ae12c6a07aedc4
dylanonelson <dylanonelson@gmail.com>
2017-09-11 22:44:53 -0400

    Handle empty state for log types

--- src/redux-modules/logTypes/actions.js
+++ src/redux-modules/logTypes/actions.js
@@ -5,6 +5,8 @@ const INGEST_LOG_TYPES = 'logTypes/INGEST_LOG_TYPES';
 export const ingestLogTypes = createAction(
   INGEST_LOG_TYPES,
   ({ logTypes }) => {
+    if (logTypes == null) return {};
+
     return Object.keys(logTypes).reduce((memo, logTypeId) => {
       memo[logTypeId] = Object.assign(logTypes[logTypeId], { id: logTypeId });
       return memo;
