664093e421acb131f84cf77cce50351056ef1bd5
dylanonelson <dylanonelson@gmail.com>
2017-10-02 22:51:00 -0400

    Alphabetize imports

--- src/init/index.js
+++ src/init/index.js
@@ -2,12 +2,12 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import './index.css';
 import './firebaseInit';
+import './index.css';
 import './localStorageInit';
-import { authenticateUser  } from 'redux-modules/init/actions';
 import App from './root-component';
 import store from '../redux-store';
+import { authenticateUser  } from 'redux-modules/init/actions';
 
 const renderApp = () => {
   injectTapEventPlugin();
